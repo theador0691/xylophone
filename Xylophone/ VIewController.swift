5685237a4d5d657374aa1460a3ae1ccb3012382b
Andy Taylor <Theador0691@gmail.com>
2018-01-28 19:47:57 +0000

    Finished app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
+    
+    var player: AVAudioPlayer?
+    var resource: String = ""
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: resource, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
 
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,8 +43,9 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
+        resource = "note\(sender.tag)"
+        playSound()
+        print(sender.tag)
         
     }
     
